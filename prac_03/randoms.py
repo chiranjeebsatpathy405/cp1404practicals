f98f5e3bbd02ba0499f2e207a3b819291dc3b9ec
chiranjeebsatpathy405 <chiranjeeb.satpathy@my.jcu.edu.au>
2025-10-05 16:56:08 +0800

    Random function exercise is completed.

---
+++ prac_03/randoms.py
@@ -0,0 +1,9 @@
+import random
+
+#Random Function
+print(random.randint(15, 30))  # between 15 to 30
+print(random.randrange(30, 40, 2))  # between 30 and 40
+print(random.uniform(40.5, 55.5))  # between 40.5 and 55.5
+
+num_between1_100 = random.randint(1, 100)
+print(num_between1_100)
