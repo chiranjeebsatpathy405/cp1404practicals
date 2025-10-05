c59207de25ec497a8b55eeed174dd319c4f8c182
chiranjeebsatpathy405 <chiranjeeb.satpathy@my.jcu.edu.au>
2025-10-05 19:06:21 +0800

    Line1, 2, 3 output observation completed

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,9 +1,16 @@
 import random
 
 #Random Function
-print(random.randint(15, 30))  # between 15 to 30
-print(random.randrange(30, 40, 2))  # between 30 and 40
-print(random.uniform(40.5, 55.5))  # between 40.5 and 55.5
+print(random.randint(5, 20))  # line 1
+#Smallest number: 5
+#Largest number: 20
 
-num_between1_100 = random.randint(1, 100)
-print(num_between1_100)
+
+print(random.randrange(3, 10, 2))  # line 2
+#Smallest number: 3
+#Largest number: 9
+
+print(random.uniform(2.5, 5.5))  # line 3
+#Smallest possible number: 2.5
+#Largest possible number: 5.5
+
