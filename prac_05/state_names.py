a307d46c850ed9d9a8bf333b3cbf572be9ea9f64
chiranjeebsatpathy405 <chiranjeeb.satpathy@my.jcu.edu.au>
2025-10-19 16:37:28 +0800

    states and names neatly lined up with string formatting
    "Easier to Ask Forgiveness than Permission" (EAFP) approach

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,11 +1,12 @@
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
+for code in ["NSW", "QLD", "NT", "WA", "ACT", "VIC", "TAS", "SA", "XYZ"]:
+    try:
+        # Try to get the state name
+        name = CODE_TO_NAME[code]
+        print(f"{code:<3} is {name}")
+    except KeyError:
+        # Handle missing key
+        print(f"{code:<3} is Unknown")
+
