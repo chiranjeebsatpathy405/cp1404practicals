fbc988e7eee398ffbcbfafed20724b78135ddf87
chiranjeebsatpathy405 <chiranjeeb.satpathy@my.jcu.edu.au>
2025-10-19 17:48:34 +0800

    program to count the occurrences of words in a string

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,33 @@
+# Input any string
+text = input("Enter a string: ")
+
+# lowercase conversion to make case insensitive  and split into words
+words = text.lower().split()
+
+# empty dictionary creation as word_counts to store word counts
+word_counts = {}
+
+# Count each word
+for word in words:
+    # Remove  commas, periods, etc.
+    word = word.strip(".,!?")
+    if word in word_counts:
+        word_counts[word] += 1
+    else:
+        word_counts[word] = 1
+
+# show the counts
+print("\nWord occurrences:")
+for word, count in word_counts.items():
+    print(f"{word}: {count}")
+
+    # Enter a string: this is a collection of words of nice words this is a fun thing it is
+    # Word occurrences:
+   # this: 2
+    #is: 3
+    #a: 2
+    #collection: 1
+   # of: 2
+   # words: 2
+   # nice: 1
+   # fun: 1
