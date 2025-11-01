6a4841db166e75e61f3fab7a7bd56981e2b80acb
chiranjeebsatpathy405 <chiranjeeb.satpathy@my.jcu.edu.au>
2025-11-02 07:59:58 +0800

    limo object created with add_fuel method and use the limo.drive as per the assignment

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -6,23 +6,22 @@ This is why we name our folders with no spaces or capitals, as valid module name
 
 from prac_06.car import Car
 
-
 def main():
-    """Demo test code to show how to use car class."""
-    my_car = Car(180)
-    my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
-    print(my_car)
-    # New  'limo' Car object called
-    limo = Car(100)
-    print(f"Limo created with {limo.fuel} units of fuel.")
+
+    # New 'limo' Car object is created
+    limo = Car("Limo", 100)
+    print(f"Limo car created with {limo.fuel} units of fuel.")
+
     # Add 20 more units of fuel with add_fuel() method
     limo.add_fuel(20)
     print(f"After adding fuel, limo now has {limo.fuel} units of fuel.")
-    # Print the fuel amount in the car
-    print(f"Limo has now  {limo.fuel} units of fuel.")
-    # drive the car 115 km
+
+    # Drive the car for 115 km
     distance_driven = limo.drive(115)
+    print(f"Limo actually drove {distance_driven} km.")
 
+    # Now Print the car object to trigger the __str__ method
+    print(limo)
 
-main()
+if __name__ == "__main__":
+    main()
