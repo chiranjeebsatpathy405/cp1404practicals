71cd6fecbe5f627fa2d6ff70d4965ccddfec99b8
chiranjeebsatpathy405 <chiranjeeb.satpathy@my.jcu.edu.au>
2025-11-15 18:06:30 +0800

    Added button handler and tested the GUI functionalities

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,5 +8,9 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print('greet')
+        self.root.ids.output_label.text = "Hello "
 
 BoxLayoutDemo().run()
+
