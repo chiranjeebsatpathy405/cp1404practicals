4a69dd1dc4d2c2120161334a2d40a4e5e2516d1d
chiranjeebsatpathy405 <chiranjeeb.satpathy@my.jcu.edu.au>
2025-11-23 12:35:08 +0800

    Added class variable and changed the init function

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -7,16 +7,16 @@ from prac_09.car import Car
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
-
-    def __init__(self, name, fuel, price_per_km):
+    price_per_km = 1.23 # class variable shared by all taxis
+    def __init__(self, name, fuel) :
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
+        #self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
         """Return a string like a Car but with current fare distance."""
-        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare"
 
     def get_fare(self):
         """Return the price for the taxi trip."""
@@ -24,10 +24,12 @@ class Taxi(Car):
 
     def start_fare(self):
         """Begin a new fare."""
-        self.current_fare_distance = 0
+        #self.current_fare_distance = 0
+        """Return the fare based on distance driven."""
+        return round(self.price_per_km * self.current_fare_distance, 2)
 
     def drive(self, distance):
-        """Drive like parent Car but calculate fare distance as well."""
+        #track fare distance
         distance_driven = super().drive(distance)
         self.current_fare_distance += distance_driven
         return distance_driven
