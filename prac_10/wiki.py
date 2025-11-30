e6debeec34ec87ca6fb95cb38fb64b28d8ec8834
chiranjeebsatpathy405 <chiranjeeb.satpathy@my.jcu.edu.au>
2025-11-30 20:10:53 +0800

    Messages formatted

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -14,10 +14,10 @@ def main():
             print(page.summary[:500] + "...\n")
             print(page.url + "\n")
         except wikipedia.DisambiguationError as e:
-            print("\nWe need a more specific title. Try a new search:\n")
+            print("\nWe need a more specific title. Try one of the following, or a new search:\n")
             print(e.options[:15], "...\n")
         except wikipedia.PageError:
-            print(f'\nPage id "{title}" does not match any pages. Try some other id!\n')
+            print(f'\nPage id "{title}" does not match any pages.Try another id!\n')
         except Exception as e:
             print(f"\nunexpected error occurred: {e}\n")
 
